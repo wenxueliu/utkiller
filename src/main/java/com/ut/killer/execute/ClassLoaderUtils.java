93cdb2c4011860016bb3940bdead13394aa0ab15
wenxueliu <wenxueliu73@163.com>
2024-05-23 00:11:28 +0800

    add vmtool

---
+++ src/main/java/com/ut/killer/execute/ClassLoaderUtils.java
@@ -0,0 +1,178 @@
+package com.ut.killer.execute;
+
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author hengyunabc 2019-02-05
+ *
+ */
+public class ClassLoaderUtils {
+
+    public static Set<ClassLoader> getAllClassLoader(Instrumentation inst) {
+        Set<ClassLoader> classLoaderSet = new HashSet<ClassLoader>();
+
+        for (Class<?> clazz : inst.getAllLoadedClasses()) {
+            ClassLoader classLoader = clazz.getClassLoader();
+            if (classLoader != null) {
+                classLoaderSet.add(classLoader);
+            }
+        }
+        return classLoaderSet;
+    }
+
+    public static ClassLoader getClassLoader(Instrumentation inst, String hashCode) {
+        if (hashCode == null || hashCode.isEmpty()) {
+            return null;
+        }
+
+        for (Class<?> clazz : inst.getAllLoadedClasses()) {
+            ClassLoader classLoader = clazz.getClassLoader();
+            if (classLoader != null) {
+                if (Integer.toHexString(classLoader.hashCode()).equals(hashCode)) {
+                    return classLoader;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 通过类名查找classloader
+     * @param inst
+     * @param classLoaderClassName
+     * @return
+     */
+    public static List<ClassLoader> getClassLoaderByClassName(Instrumentation inst, String classLoaderClassName) {
+        if (classLoaderClassName == null || classLoaderClassName.isEmpty()) {
+            return null;
+        }
+        Set<ClassLoader> classLoaderSet = getAllClassLoader(inst);
+        List<ClassLoader> matchClassLoaders = new ArrayList<ClassLoader>();
+        for (ClassLoader classLoader : classLoaderSet) {
+            if (classLoader.getClass().getName().equals(classLoaderClassName)) {
+                matchClassLoaders.add(classLoader);
+            }
+        }
+        return matchClassLoaders;
+    }
+
+    public static String classLoaderHash(ClassLoader classLoader) {
+        int hashCode = 0;
+        if (classLoader == null) {
+            hashCode = System.identityHashCode(classLoader);
+        } else {
+            hashCode = classLoader.hashCode();
+        }
+        if (hashCode <= 0) {
+            hashCode = System.identityHashCode(classLoader);
+            if (hashCode < 0) {
+                hashCode = hashCode & Integer.MAX_VALUE;
+            }
+        }
+        return Integer.toHexString(hashCode);
+    }
+
+    /**
+     * Find List<ClassLoader> by the class name of ClassLoader or the return value of ClassLoader#toString().
+     * @param inst
+     * @param classLoaderClassName
+     * @param classLoaderToString
+     * @return
+     */
+    public static List<ClassLoader> getClassLoader(Instrumentation inst, String classLoaderClassName, String classLoaderToString) {
+        List<ClassLoader> matchClassLoaders = new ArrayList<ClassLoader>();
+        if (StringUtils.isEmpty(classLoaderClassName) && StringUtils.isEmpty(classLoaderToString)) {
+            return matchClassLoaders;
+        }
+        Set<ClassLoader> classLoaderSet = getAllClassLoader(inst);
+        List<ClassLoader> matchedByClassLoaderToStr = new ArrayList<ClassLoader>();
+        for (ClassLoader classLoader : classLoaderSet) {
+            // only classLoaderClassName
+            if (!StringUtils.isEmpty(classLoaderClassName) && StringUtils.isEmpty(classLoaderToString)) {
+                if (classLoader.getClass().getName().equals(classLoaderClassName)) {
+                    matchClassLoaders.add(classLoader);
+                }
+            }
+            // only classLoaderToString
+            else if (!StringUtils.isEmpty(classLoaderToString) && StringUtils.isEmpty(classLoaderClassName)) {
+                if (classLoader.toString().equals(classLoaderToString)) {
+                    matchClassLoaders.add(classLoader);
+                }
+            }
+            // classLoaderClassName and classLoaderToString
+            else {
+                if (classLoader.getClass().getName().equals(classLoaderClassName)) {
+                    matchClassLoaders.add(classLoader);
+                }
+                if (classLoader.toString().equals(classLoaderToString)) {
+                    matchedByClassLoaderToStr.add(classLoader);
+                }
+            }
+        }
+        // classLoaderClassName and classLoaderToString
+        if (!StringUtils.isEmpty(classLoaderClassName) && !StringUtils.isEmpty(classLoaderToString)) {
+            matchClassLoaders.retainAll(matchedByClassLoaderToStr);
+        }
+        return matchClassLoaders;
+    }
+
+    @SuppressWarnings({ "unchecked", "restriction" })
+    public static URL[] getUrls(ClassLoader classLoader) {
+        if (classLoader instanceof URLClassLoader) {
+            return ((URLClassLoader) classLoader).getURLs();
+        }
+
+        // jdk9
+        if (classLoader.getClass().getName().startsWith("jdk.internal.loader.ClassLoaders$")) {
+            try {
+                Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+                field.setAccessible(true);
+                sun.misc.Unsafe unsafe = (sun.misc.Unsafe) field.get(null);
+
+                Class<?> ucpOwner = classLoader.getClass();
+                Field ucpField = null;
+
+                // jdk 9~15: jdk.internal.loader.ClassLoaders$AppClassLoader.ucp
+                // jdk 16~17: jdk.internal.loader.BuiltinClassLoader.ucp
+                while (ucpField == null && !ucpOwner.getName().equals("java.lang.Object")) {
+                    try {
+                        ucpField = ucpOwner.getDeclaredField("ucp");
+                    } catch (NoSuchFieldException ex) {
+                        ucpOwner = ucpOwner.getSuperclass();
+                    }
+                }
+                if (ucpField == null) {
+                    return null;
+                }
+
+                long ucpFieldOffset = unsafe.objectFieldOffset(ucpField);
+                Object ucpObject = unsafe.getObject(classLoader, ucpFieldOffset);
+                if (ucpObject == null) {
+                    return null;
+                }
+
+                // jdk.internal.loader.URLClassPath.path
+                Field pathField = ucpField.getType().getDeclaredField("path");
+                if (pathField == null) {
+                    return null;
+                }
+                long pathFieldOffset = unsafe.objectFieldOffset(pathField);
+                ArrayList<URL> path = (ArrayList<URL>) unsafe.getObject(ucpObject, pathFieldOffset);
+
+                return path.toArray(new URL[path.size()]);
+            } catch (Throwable e) {
+                // ignore
+                return null;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/ut/killer/execute/InstanceUtils.java
@@ -0,0 +1,165 @@
+package com.ut.killer.execute;
+
+
+import arthas.VmTool;
+import com.alibaba.bytekit.utils.IOUtils;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.instrument.Instrumentation;
+import java.security.CodeSource;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstanceUtils {
+    private static final Logger logger = LoggerFactory.getLogger(InstanceUtils.class);
+
+    private String className;
+
+    private String hashCode = null;
+    private String classLoaderClass;
+
+    private String libPath;
+    private static String defaultLibPath;
+    private static VmTool vmTool = null;
+
+    static {
+        String libName = VmToolUtils.detectLibName();
+        if (libName != null) {
+            CodeSource codeSource = InstanceUtils.class.getProtectionDomain().getCodeSource();
+            if (codeSource != null) {
+                try {
+                    File bootJarPath = new File(codeSource.getLocation().toURI().getSchemeSpecificPart());
+                    File soFile = new File(bootJarPath.getParentFile(), "lib" + File.separator + libName);
+                    if (soFile.exists()) {
+                        defaultLibPath = soFile.getAbsolutePath();
+                    }
+                } catch (Throwable e) {
+                    logger.error("can not find VmTool so", e);
+                }
+            }
+        }
+
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public void setHashCode(String hashCode) {
+        this.hashCode = hashCode;
+    }
+
+    public void setClassLoaderClass(String classLoaderClass) {
+        this.classLoaderClass = classLoaderClass;
+    }
+
+    public void setLibPath(String path) {
+        libPath = path;
+    }
+
+    public Object getInstance() {
+        try {
+//            Instrumentation inst = HotSwapAgentMain.startAgentAndGetInstrumentation();
+            Instrumentation inst = ByteBuddyAgent.install();
+            if (className == null) {
+                throw new IllegalArgumentException("The className option cannot be empty!");
+            }
+            ClassLoader classLoader = null;
+            if (hashCode != null) {
+                classLoader = ClassLoaderUtils.getClassLoader(inst, hashCode);
+                if (classLoader == null) {
+                    throw new IllegalArgumentException("Can not find classloader with hashCode: " + hashCode + ".");
+                }
+            } else if (classLoaderClass != null) {
+                List<ClassLoader> matchedClassLoaders = ClassLoaderUtils.getClassLoaderByClassName(inst,
+                        classLoaderClass);
+                if (matchedClassLoaders.size() == 1) {
+                    classLoader = matchedClassLoaders.get(0);
+                    hashCode = Integer.toHexString(matchedClassLoaders.get(0).hashCode());
+                } else if (matchedClassLoaders.size() > 1) {
+//                        Collection<ClassLoaderVO> classLoaderVOList = ClassUtils
+//                                .createClassLoaderVOList(matchedClassLoaders);
+
+//                        VmToolModel vmToolModel = new VmToolModel().setClassLoaderClass(classLoaderClass)
+//                                .setMatchedClassLoaders(classLoaderVOList);
+                    throw new IllegalArgumentException(
+                            "Found more than one classloader by class name, please specify classloader with '-c <classloader hash>'");
+                } else {
+                    throw new IllegalArgumentException("Can not find classloader by class name: " + classLoaderClass + ".");
+                }
+            } else {
+                classLoader = ClassLoader.getSystemClassLoader();
+            }
+
+            List<Class<?>> matchedClasses = new ArrayList<Class<?>>(
+                    SearchUtils.searchClassOnly(inst, className, false, hashCode));
+            int matchedClassSize = matchedClasses.size();
+            if (matchedClassSize == 0) {
+                throw new IllegalArgumentException("Can not find class by class name: " + className + ".");
+            } else if (matchedClassSize > 1) {
+                throw new IllegalArgumentException("Found more than one class: " + matchedClasses + ", please specify classloader with '-c <classloader hash>'");
+            } else {
+                Object[] instances = vmToolInstance().getInstances(matchedClasses.get(0));
+                if (instances.length > 1) {
+                    throw new IllegalArgumentException("Found more than one instance of class: " + matchedClasses.get(0) + ", please specify classloader with '-c <classloader hash>'");
+                }
+                Object value = instances[0];
+                return value;
+            }
+        } catch (Throwable e) {
+            logger.error("vmtool error", e);
+            throw new IllegalArgumentException("vmtool error: " + e.getMessage());
+        }
+    }
+
+    private VmTool vmToolInstance() {
+        if (vmTool != null) {
+            return vmTool;
+        } else {
+            if (libPath == null) {
+                libPath = defaultLibPath;
+            }
+
+            // 尝试把lib文件复制到临时文件里，避免多次attach时出现 Native Library already loaded in another classloader
+            FileOutputStream tmpLibOutputStream = null;
+            FileInputStream libInputStream = null;
+            try {
+                File tmpLibFile = File.createTempFile(VmTool.JNI_LIBRARY_NAME, null);
+                tmpLibOutputStream = new FileOutputStream(tmpLibFile);
+                libInputStream = new FileInputStream(libPath);
+
+                IOUtils.copy(libInputStream, tmpLibOutputStream);
+                libPath = tmpLibFile.getAbsolutePath();
+                logger.debug("copy {} to {}", libPath, tmpLibFile);
+            } catch (Throwable e) {
+                logger.error("try to copy lib error! libPath: {}", libPath, e);
+            } finally {
+                IOUtils.close(libInputStream);
+                IOUtils.close(tmpLibOutputStream);
+            }
+
+            vmTool = VmTool.getInstance(libPath);
+        }
+        return vmTool;
+    }
+
+    public static Object springContextInstance() {
+        InstanceUtils vmToolCommand = new InstanceUtils();
+        vmToolCommand.setClassLoaderClass("org.springframework.boot.loader.LaunchedURLClassLoader");
+        vmToolCommand.setClassName("org.springframework.context.ApplicationContext");
+        return vmToolCommand.getInstance();
+    }
+
+    public static Object springContextInstance(String className) {
+        InstanceUtils instanceUtils = new InstanceUtils();
+        instanceUtils.setClassLoaderClass("sun.misc.Launcher$AppClassLoader");
+//        vmToolCommand.setClassLoaderClass("org.springframework.boot.loader.LaunchedURLClassLoader");
+        instanceUtils.setClassName(className);
+        return instanceUtils.getInstance();
+    }
+}
